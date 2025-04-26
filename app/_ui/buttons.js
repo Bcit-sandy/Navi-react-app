e650231b726014903cf9ec7e43567fb7c08808e5
daniel-liu01 <mliu192@my.bcit.ca>
2025-04-25 22:58:49 -0700

    Setup / testing explore button

--- app/_ui/buttons.js
+++ app/_ui/buttons.js
@@ -1,14 +1,29 @@
-import "./module.button.css";
-import "./globals.css";
 
-// Sign up 
+import "./module.button.css";
+import "../globals.css";
+
+
+
+// Sign up
 // Onboarding
 
+// Let's go Explore | big button
 
-// HomePage 
+export function Explore() {
+    return <button>Let's Go Explore</button>;
+  }
+
+// HomePage
+
+// NavBar - template
 
 // Profile
 
 // Create Event
 
 // Setting
+
+//Exports
+// buttons.js
+// export const Navbar = () => <nav>Navbar</nav>;
+
