764cc822f8238ad78ac10e1f3a1a378329bd5458
daniel-liu01 <mliu192@my.bcit.ca>
2025-05-03 15:14:47 -0700

    Update page.js imports

--- app/page.js
+++ app/page.js
@@ -1,92 +1,11 @@
 "use client";
+import { useEffect, useState, useRef } from "react";
 
 import Image from "next/image";
-import {
-    Explore,
-    Skip,
-    Add,
-    Community,
-    Event,
-    Elipses,
-    Report,
-    ImageAdd,
-    EventNotification,
-    RoundNext,
-    LeaveEvent,
-    SettingsButton,
-    ParticipantField,
-    EditBioText,
-    PlaceholderImg_sm, 
-    PlaceholderImg_lg,
-    PlaceholderImg_event,
-    PlaceholderImg_featured,
-    PlaceholderImg_profile,
-    PlaceholderImg_profile_sm,
-    Share,
-    PopupSlider,
-    SignUp_Input
-} from "./_ui/buttons";
+import * as UI from "./_ui/buttons"; 
+import {EventsPostTextField,CommunityPostTextField } from "./_ui/textfield"
 
 export default function Page() {
-    return (
-        <main>
-             <SignUp_Input label="Email" type="email" name="email" placeholder="Enter email"/>
-        <SignUp_Input label="Password" type="password" name="password" placeholder="Enter password" />
-        <SignUp_Input
-          label="Re-enter Password"
-          type="password"
-          name="repassword"
-          placeholder="Re-enter password"
-        />
-            <Share />
-            <PlaceholderImg_profile_sm />
-            <PlaceholderImg_profile />
-            <PlaceholderImg_event />
-            <PlaceholderImg_featured />
-            <PlaceholderImg_lg />
-            <PlaceholderImg_sm />
-            <EditBioText />
-            <ParticipantField />
-            <SettingsButton />
-            <Explore />
-            <Skip />
-            <Add />
-            <Community />
-            <Event />
-            <Elipses />
-            <Report />
-            <ImageAdd />
-            <EventNotification />
-            <RoundNext />
-            <LeaveEvent />
-        </main>
-        
-    );
-}
-
-import Image from "next/image";
-import {
-    AnonymousToggle,
-    Search,
-    Profile,
-    Comment,
-    Edit,
-    Delete,
-    Toggle,
-    HomePageNav,
-    TakePhotoButton,
-    ChoosePhotoButton,
-    UserCircles,
-    StudentCategories,
-    SingleComment,
-    CommentWithReply,
-    LocationDropdown,
-    StudentTypeDropdown,
-} from "./_ui/buttons";
-import { EventsPostTextField, CommunityPostTextField } from "./_ui/textfield";
-import { useState } from "react";
-
-export default function Home() {
     const [toggled, setToggled] = useState(false);
     const [onOffToggled, setonOffToggled] = useState(false);
     const [selectedCategory, setSelectedCategory] = useState("");
@@ -98,31 +17,63 @@ export default function Home() {
     };
     return (
         <main>
-            <AnonymousToggle />
-            <Search />
-            <Profile />
-            <Comment />
-            <Edit />
-            <Delete />
-            <Toggle />
-            <HomePageNav />
-            <TakePhotoButton />
-            <ChoosePhotoButton />
-            <UserCircles users={users} />
-            <StudentCategories onSelect={handleCategorySelect} />
+        <UI.SignUp_Input label="Email" type="email" name="email" placeholder="Enter email"/>
+        <UI.SignUp_Input label="Password" type="password" name="password" placeholder="Enter password" />
+        <UI.SignUp_Input
+          label="Re-enter Password"
+          type="password"
+          name="repassword"
+          placeholder="Re-enter password"
+        />
+        <UI.AnonymousToggle />
+            <UI.Search />
+            <UI.Profile />
+            <UI.Comment />
+            <UI.Edit />
+            <UI.Delete />
+            <UI.Toggle />
+            <UI.HomePageNav />
+            <UI.TakePhotoButton />
+            <UI.ChoosePhotoButton />
+            <UI.UserCircles users={users} />
+            <UI.StudentCategories onSelect={handleCategorySelect} />
             <CommunityPostTextField
                 value={post}
                 onChange={(e) => setPost(e.target.value)}
             />
             <EventsPostTextField />
-            <SingleComment
+            <UI.SingleComment
                 author='Jenny Pham'
                 date='Mar 31, 2025'
                 content="I would say Granville Island. I'd have a lot of fun!!"
             />
-            <CommentWithReply />
-            <LocationDropdown />
-            <StudentTypeDropdown />
+            <UI.CommentWithReply />
+            <UI.LocationDropdown />
+            <UI.StudentTypeDropdown />
+            <UI.Share />
+            <UI.PlaceholderImg_profile_sm />
+            <UI.PlaceholderImg_profile />
+            <UI.PlaceholderImg_event />
+            <UI.PlaceholderImg_featured />
+            <UI.PlaceholderImg_lg />
+            <UI.PlaceholderImg_sm />
+            <UI.EditBioText />
+            <UI.ParticipantField />
+            <UI.SettingsButton />
+            <UI.Explore />
+            <UI.Skip />
+            <UI.Add />
+            <UI.Community />
+            <UI.Event />
+            <UI.Elipses />
+            <UI.Report />
+            <UI.ImageAdd />
+            <UI.EventNotification />
+            <UI.RoundNext />
+            <UI.LeaveEvent />
         </main>
+        
     );
 }
+
+
