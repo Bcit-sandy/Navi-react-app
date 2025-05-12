f46b40983f1da5dfff7908b7d0ca2859ed310974
Bcit-sandy <mc.bcit2024@gmail.com>
2025-05-12 14:43:35 -0700

    fixing buttons

---
+++ .history/app/Buttons/Follow/index_20250512143410.js
@@ -0,0 +1,21 @@
+"use client";
+
+import { useState } from "react";
+import "./styles.css";
+
+export function Follow({ isFollowing = false, onClick }) {
+    return (
+        <button 
+            className={`follow-button ${isFollowing ? 'followed' : ''}`}
+            onClick={onClick}
+        >
+            <Image 
+                src={isFollowing ? "/followed.svg" : "/follow.svg"} 
+                alt={isFollowing ? "unfollow icon" : "follow icon"}
+                width={25}
+                height={25}
+            />
+            <span className="follow-text">{isFollowing ? "Followed" : "Follow"}</span>
+        </button>
+    );
+}
