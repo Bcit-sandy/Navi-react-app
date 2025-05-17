a0dfa4c7f46bc9559898f21b424960300ec43275
Bcit-sandy <mc.bcit2024@gmail.com>
2025-05-17 16:43:23 -0700

    add home nav bar 2 to step 1

--- app/EventHomePage/step1/page.js
+++ app/EventHomePage/step1/page.js
@@ -5,12 +5,16 @@ import styles from '@/app/EventHomePage/step1/page.module.css';
 import EventSlider from '@/app/BigComponents/EventSliders/eventslider';
 import EventPost from '@/app/BigComponents/EventPost/EventPost';
 import NavBar from '@/app/BigComponents/NavBar/navbar';
+import { HomePageNav2 } from "@/app/UiElements/HomePageNav2";
 
 const Step1 = () => {
     return (
         <main className={styles.main}>
             <div className={styles.topContainer}>
             <NavBar />
+            </div>
+            <div className={styles.homePageNav}>
+                    <HomePageNav2 />
             </div>
             <div className={styles.middleContainer}>
             <EventSlider />
---
+++ app/UiElements/HomePageNav2/index.js
@@ -0,0 +1,32 @@
+"use client";
+
+import { useState } from "react";
+import "./styles.css";
+
+// 2 buttons (community and events)
+export function HomePageNav2() {
+    const [activeLink, setActiveLink] = useState("events");
+
+    return (
+        <nav className='navbar'>
+            <div className='nav-links'>
+                <button
+                    className={`nav-link ${
+                        activeLink === "community" ? "active" : ""
+                    }`}
+                    onClick={() => setActiveLink("community")}
+                >
+                    Community
+                </button>
+                <button
+                    className={`nav-link ${
+                        activeLink === "events" ? "active" : ""
+                    }`}
+                    onClick={() => setActiveLink("events")}
+                >
+                    Events
+                </button>
+            </div>
+        </nav>
+    );
+}
