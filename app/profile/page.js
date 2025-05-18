3e8857b8bc6125aa6e2205655432e0f98843a72b
daniel-liu01 <mliu192@my.bcit.ca>
2025-05-17 22:02:26 -0700

    Set up profile page

---
+++ app/profile/page.js
@@ -0,0 +1,11 @@
+"use client";
+import styles from "@/app/profile/page.module.css";
+import Button from "@/app/button/button";
+
+export default function Profile() {
+  return (
+    <main>
+      <h1>hello</h1>
+    </main>
+  );
+}
